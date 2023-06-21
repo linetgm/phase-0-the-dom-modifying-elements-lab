e4199e1b64a2bb37c2408c5513ea51e11d22b020
Linet <muthiigathoni@gmail.com>
2023-06-21 10:11:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Justin Coppola is the champion'; 
+
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.remove();
+}
